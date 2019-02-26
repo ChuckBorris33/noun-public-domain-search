33ab1d3cf8b81f24f218adbb9f7eae7fa8f584ec
ChuckBorris33 <boris.zalman33@gmail.com>
2019-02-26 19:27:09 +0100

    Add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="app">
-    <NounIconSearch/>>
-  </div>
+  <v-app id="app">
+    <v-content>
+      <NounIconSearch />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -14,14 +16,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
+import "./plugins/vuetify";
 import App from "./App.vue";
-
-Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
