fb948b608fba948b7e1a945ba181a1743249eaec
ChuckBorris33 <boris.zalman33@gmail.com>
2019-02-26 19:27:27 +0100

    Change timeout to 5000

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,7 +4,7 @@ import VueAxios from "vue-axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5000/",
-  timeout: 1000
+  timeout: 5000
 });
 
 Vue.use(VueAxios, axiosInstance);
