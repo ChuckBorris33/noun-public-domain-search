c678d87476efcc0393d269683545e7d498c4c26c
ChuckBorris33 <boris.zalman33@gmail.com>
2019-02-26 19:27:46 +0100

    PEP8 Backend

--- backend/api.py
+++ backend/api.py
@@ -1,19 +1,19 @@
-import os
 import requests
+import os
 
-from requests_oauthlib import OAuth1
 from flask import Flask, request, Response
 from flask_cors import CORS
+from requests_oauthlib import OAuth1
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r'/api/*': {'origins': '*'}})
 
 KEY = os.environ.get('NOUN_KEY')
 SECRET = os.environ.get('NOUN_SECRET')
 
 auth = OAuth1(KEY, SECRET)
 
-BASE_URL = "http://api.thenounproject.com"
+BASE_URL = 'http://api.thenounproject.com'
 
 @app.route('/api/search_icons/<string:term>', methods=['GET'])
 def search_icons(term):
