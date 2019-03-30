42be41fa6fb58665b19966feee64067e7c5d8f7c
ChuckBorris33 <boris.zalman33@gmail.com>
2019-03-30 21:11:13 +0100

    Remove console.log

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,8 +2,6 @@ import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
-console.log(process.env);
-
 const axiosInstance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
   timeout: 5000
